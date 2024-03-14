c6f3cc00a8c82c602189ed98372767898c19fc61
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-03-14 11:01:01 -0700

    Add files via upload

---
+++ temperature_from_F_to_C.m
@@ -0,0 +1,8 @@
+%soheil mansouri (electrical engineering)
+%script file to convert temperature from F to C
+%
+F=input('temperature in degrees F:');  %give F
+C=(F-32)*5/9;
+disp(['Temperature in degrees C=',num2str(C)])  %get C
+%stop
+
